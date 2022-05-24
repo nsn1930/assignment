2907bd571ae85111e762f364d5700b1afd3f02b7
nsn1930 <pavel.brodovskij@gmail.com>
2022-05-24 21:50:07 +0200

    page CSS

--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -2,7 +2,7 @@ function Country({post, index}) {
 
     return( 
         <>
-            <tr style={{backgroundColor: (index % 2 === 0 ? '' : '#535353')}}>  
+            <tr style={{backgroundColor: (index % 2 === 0 ? '#424242' : '#535353')}}>  
                 <td>{post.name}</td>   
                 <td>{post.area}</td>   
                 <td>{post.region}</td>  
