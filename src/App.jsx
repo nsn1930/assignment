2dceecd4ffede89607c2f4cbdf169d618e3c8771
nsn1930 <pavel.brodovskij@gmail.com>
2022-05-24 17:10:31 +0200

    state initiation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+        axios.get('https://restcountries.com/v2/all?fields=name,region,area')
+        .then(res => {
+            setPosts(res.data);
+        })
+    }, []);
+
   return (
     <div className="App">
       <header className="App-header">
